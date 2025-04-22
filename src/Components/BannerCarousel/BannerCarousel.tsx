34fddbb9508a083d9726640be4f529c3e1fde439
luizevrs <luiizevrsj@gmail.com>
2025-04-22 08:37:32 -0300

    feat: responsividade Header mob

--- src/Components/BannerCarousel/BannerCarousel.tsx
+++ src/Components/BannerCarousel/BannerCarousel.tsx
@@ -2,28 +2,48 @@
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from "react";
 import { ChevronLeft, ChevronRight } from "lucide-react";
-import img1 from "../../../public/assets/images/promocao1.png";
-import img2 from "../../../public/assets/images/promocao2.png";
-import img3 from "../../../public/assets/images/promocao3.png";
+
+import mobile1 from "../../../public/assets/images/mobilepromo1.png";
+import mobile2 from "../../../public/assets/images/mobilepromo2.png";
+import mobile3 from "../../../public/assets/images/mobilepromo3.png";
+
+import desktop1 from "../../../public/assets/images/promocao1.png";
+import desktop2 from "../../../public/assets/images/promocao2.png";
+import desktop3 from "../../../public/assets/images/promocao3.png";
+
 import "./BannerCarousel.css";
 
-const banners = [img1, img2, img3];
+const mobileBanners = [mobile1, mobile2, mobile3];
+const desktopBanners = [desktop1, desktop2, desktop3];
 
 export default function BannerCarousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const [isDesktop, setIsDesktop] = useState(false);
 
   const nextSlide = () => {
-    setCurrentIndex((prev) => (prev + 1) % banners.length);
+    setCurrentIndex((prev) => (prev + 1) % mobileBanners.length);
   };
 
   const prevSlide = () => {
-    setCurrentIndex((prev) => (prev === 0 ? banners.length - 1 : prev - 1));
+    setCurrentIndex((prev) => (prev === 0 ? mobileBanners.length - 1 : prev - 1));
   };
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth >= 1024);
+    };
+
+    handleResize(); // Executa na montagem
+    window.addEventListener("resize", handleResize);
     const interval = setInterval(nextSlide, 7000);
-    return () => clearInterval(interval);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+      clearInterval(interval);
+    };
   }, []);
+
+  const banners = isDesktop ? desktopBanners : mobileBanners;
 
   return (
     <div className="carousel-container">
@@ -37,10 +57,10 @@ export default function BannerCarousel() {
       ))}
 
       <button onClick={prevSlide} className="carousel-btn left">
-        <ChevronLeft size={24} />
+        <ChevronLeft size={20} />
       </button>
       <button onClick={nextSlide} className="carousel-btn right">
-        <ChevronRight size={24} />
+        <ChevronRight size={20} />
       </button>
 
       <div className="carousel-dots">
